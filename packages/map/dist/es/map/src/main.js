d61595f9ba0865ff0e988c690803b8c8b53839d9
panxiaoliang <panxiaoliang>
2024-01-28 20:40:51 +0800

    fix: init

--- packages/map/dist/es/map/src/main.js
+++
@@ -1,9 +0,0 @@
-import { isArray } from '../../utils/dist/es/isArray/index.js';
-
-var map = function (data, fn) {
-    if (isArray(data)) {
-        return data.map(function (i) { return fn(i); });
-    }
-};
-
-export { map };
